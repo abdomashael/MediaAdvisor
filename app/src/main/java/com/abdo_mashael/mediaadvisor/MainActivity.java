802109bd286a01d0751f4641688514774905ee17
abdomashael <abdomashael@yahoo.com>
2017-04-15 14:48:41 +0200

    final

--- app/src/main/java/com/abdo_mashael/mediaadvisor/MainActivity.java
+++ app/src/main/java/com/abdo_mashael/mediaadvisor/MainActivity.java
@@ -9,6 +9,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String situation, response,solution;
+    private int envirSpin,jobSpin,feedbackSpin;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,32 +24,72 @@ public class MainActivity extends AppCompatActivity {
 
             //Get a ref to the Spinner
             Spinner envir = (Spinner) findViewById(R.id.environment);
+            Spinner job = (Spinner) findViewById(R.id.job);
+            Spinner feedback = (Spinner) findViewById(R.id.feedback);
+
+
+            //Get a ref to the TextView
+            TextView sit_solve = (TextView) findViewById(R.id.sit_solve);
+            TextView resp_solve = (TextView) findViewById(R.id.resp_solve);
+            TextView media = (TextView) findViewById(R.id.solution);
+
+
+            situation=savedInstanceState.getString("situation");
+            response=savedInstanceState.getString("response");
+            solution =savedInstanceState.getString("solution");
+            envirSpin =savedInstanceState.getInt("envirSpin");
+            jobSpin =savedInstanceState.getInt("jobSpin");
+            feedbackSpin =savedInstanceState.getInt("feedbackSpin");
+
+            envir.setSelection(envirSpin);
+            job.setSelection(jobSpin);
+            feedback.setSelection(feedbackSpin);
+
+            sit_solve.setText(situation);
+            resp_solve.setText(response);
+            media.setText(solution);
+
 
             GridLayout layout1 = (GridLayout) findViewById(R.id.layout1);
             layout1.setVisibility(View.VISIBLE);
 
-            stiSituation Situation = new stiSituation();
-            Situation.situation();
-            stiReponse Response = new stiReponse();
-            Response.response();
-            mediaSolution Solution = new mediaSolution(Situation.set, Response.set);
-            Solution.setSolution();
+
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+
+        savedInstanceState.putString("situation",situation);
+        savedInstanceState.putString("response",response);
+        savedInstanceState.putString("solution",solution);
+        savedInstanceState.putInt("envirSpin",envirSpin);
+        savedInstanceState.putInt("jobSpin",jobSpin);
+        savedInstanceState.putInt("feedbackSpin",feedbackSpin);
+
+
     }
 
     public void onClickFind(View view) {
         GridLayout layout1 = (GridLayout) findViewById(R.id.layout1);
-        layout1.setVisibility(View.VISIBLE);
-
+        if (layout1.getVisibility()==View.GONE)
+            layout1.setVisibility(View.VISIBLE);
 
         stiSituation Situation = new stiSituation();
         Situation.situation();
+        situation=Situation.getSet();
+        envirSpin=Situation.getEnvirTypePositon();
 
         stiReponse Response = new stiReponse();
         Response.response();
+        response=Response.getSet();
+        jobSpin=Response.getJobTypePostion();
 
         mediaSolution Solution = new mediaSolution(Situation.set, Response.set);
         Solution.setSolution();
+        solution=Solution.getSet();
+        feedbackSpin=Solution.getFeedbackTyprPostion();
+
 
 
     }
@@ -55,17 +99,17 @@ public class MainActivity extends AppCompatActivity {
         Spinner envir = (Spinner) findViewById(R.id.environment);
         //Get the selected item in the Spinner
         String envirType = String.valueOf(envir.getSelectedItem());
-        String set;
-        int position;
         //Get a ref to the TextView
         TextView sit_solve = (TextView) findViewById(R.id.sit_solve);
+
+        String set;
 
 
         public String getSet() {
             return set;
         }
 
-        public int getPosition() {
+        public int getEnvirTypePositon() {
             return envir.getSelectedItemPosition();
         }
 
@@ -74,7 +118,8 @@ public class MainActivity extends AppCompatActivity {
             if (envirType.equals("papers") || envirType.equals("manuals") || envirType.equals("documents") || envirType.equals("textbooks")) {
                 set = "verbal";
                 sit_solve.setText(set);
-            }//Rule 2
+            }
+            //Rule 2
             if (envirType.equals("pictures") || envirType.equals("illustrations") || envirType.equals("photographs") || envirType.equals("diagrams")) {
                 set = "visual";
                 sit_solve.setText(set);
@@ -97,14 +142,18 @@ public class MainActivity extends AppCompatActivity {
     private class stiReponse {
         //Get a ref to the Spinner
         Spinner job = (Spinner) findViewById(R.id.job);
-        //Get the selected item in the Spinner
+        //Get a ref to the TextView
         String jobType = String.valueOf(job.getSelectedItem());
-        String set;
         //Get a ref to the TextView
         TextView resp_solve = (TextView) findViewById(R.id.resp_solve);
 
+        String set;
+
         public String getSet() {
             return set;
+        }
+        public int getJobTypePostion(){
+            return job.getSelectedItemPosition();
         }
 
         private void response() {
@@ -137,10 +186,11 @@ public class MainActivity extends AppCompatActivity {
     private class mediaSolution {
         //Get a ref to the Spinner
         Spinner feedback = (Spinner) findViewById(R.id.feedback);
-        //Get the selected item in the Spinner
+        //Get a ref to the TextView
         String feedbackType = String.valueOf(feedback.getSelectedItem());
         //Get a ref to the TextView
         TextView solution = (TextView) findViewById(R.id.solution);
+
         String sit, resp, set;
 
         private mediaSolution(String sit, String resp) {
@@ -150,6 +200,10 @@ public class MainActivity extends AppCompatActivity {
 
         public String getSet() {
             return set;
+        }
+
+        public int getFeedbackTyprPostion(){
+            return feedback.getSelectedItemPosition();
         }
 
 
